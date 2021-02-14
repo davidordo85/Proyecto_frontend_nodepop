873906d6fa4383112a379bd5742cb152ba6040ea
David Ordoñez <davidordonezescacena@hotmail.es>
2021-02-14 17:16:21 +0100

    modificación estilo del style.css

--- frontend_javascript/js/views.js
+++ frontend_javascript/js/views.js
@@ -1,7 +1,23 @@
 export const adsView = (advertisement) => {
-    return `<div class="post">
-    <strong class="author">${advertisement.author}</strong>
-    <p class="message">${advertisement.message}</p>
+
+    return `<div class="card">
+    <div class="card-content">
+    <div class="media">
+    <div class="media-content">
+    <p class="title is-4">${advertisement.author}</p>
+    </div>
+    </div>
+    
+    <div class="content">
+    ${advertisement.message}
+    <br>
     <time datetime="${advertisement.date}">${advertisement.date}</time>
-</div>`;
+    </div>
+    </div>
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+      </figure>
+    </div>
+  </div>`
 }
